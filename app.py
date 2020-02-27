3b2ea835bf8f24f817cd2a74c0c6529115e28d1c
Mateusz Jamrowski <kermaxpl@gmail.com>
2020-02-27 21:48:17 +0100

    add app.server to get heroku working

--- app.py
+++ app.py
@@ -12,6 +12,9 @@ import pandas as pd
 external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
 
 app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
+
+server = app.server
+
 app.config.suppress_callback_exceptions = True
 
 app.layout = html.Div([
