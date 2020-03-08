023424d6e790bac572fa285070cb6cffda670c93
Mateusz Jamrowski <kermaxpl@gmail.com>
2020-03-08 10:44:12 +0100

    Added JSONEncoder class and from_json method. Finding x with residuals and y value is now possible

--- regression.py
+++ regression.py
@@ -1,18 +1,21 @@
 from scipy.optimize import curve_fit
 import numpy as np
 
+import json
+from json import JSONEncoder,   JSONDecoder
 
-class FourParametricLogistic:
-    def __func(self, x, A, B, C ,D):
-        """4PL lgoistic equation."""
+class FourParametricLogistic():
+    def __func(x, A, B, C, D):
+        if C == 0 or B == 0:
+            return np.inf
         return ((A-D)/(1.0+((x/C)**B))) + D
 
     def fit(self, x, y):
-        self.residuals, _ = curve_fit(self.__func, x, y)
+        self.residuals, _ = curve_fit(FourParametricLogistic.__func, x, y)
         return self.residuals
 
     def predict(self, x):
-        return self.__func(x, *self.residuals)
+        return FourParametricLogistic.__func(x, *self.residuals)
 
     def r2(self, x, y):
         x = np.array(x)
@@ -22,3 +25,24 @@ class FourParametricLogistic:
         ss_tot = sum((y - np.mean(y))**2)
 
         return 1 - (ss_res/ss_tot)
+
+    def from_json(json_data):
+        data = json.loads(json_data)
+        if 'residuals' in data:
+            model = FourParametricLogistic()
+            model.residuals = np.array(data['residuals'])
+            return model
+
+    def solve(self, y):
+        A, B, C, D = self.residuals
+        return C*((((A-D)/(y-D))-1)**(1/B))
+
+
+class FourParametricLogisticEncoder(JSONEncoder):
+    def default(self, o):
+        if isinstance(o, np.ndarray):
+            return o.tolist()
+        return o.__dict__
+
+
+
