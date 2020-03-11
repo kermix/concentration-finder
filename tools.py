613c2e8f1e094d48de674f7963f6980b4b5672b5
Mateusz Jamrowski <kermaxpl@gmail.com>
2020-03-11 13:35:40 +0100

    Cahnges in color mixing

--- tools.py
+++ tools.py
@@ -6,11 +6,8 @@ def create_and_mix_color_scale(n_colors, color_scale=dict(size='12', type='qual'
 
     colors = cl.to_rgb(cl.interp(colors_scale, n_colors))
 
-    half_index = len(colors)//2
-
-    h1, h2 = colors[:half_index], colors[half_index::][::-1]
-
-    for i, h2i in enumerate(h2):
-        h1 = h1[:i * 2] + [h2i] + h1[i * 2:]
-
-    return h1
+    n = n_colors // int(cs_size)
+    mixed = []
+    for i in range(n):
+        mixed += colors[i::n]
+    return [c for c in set(mixed)]
